2b68b8bbc186d76b4cfc47ee8d89620c8005de2c
Gabriel Andrade <gabriel.andrade@ccc.ufcg.edu.br>
2015-10-08 22:36:17 -0300

    Configurações iniciais do projeto

---
+++ src/maisPop/Facade.java
@@ -0,0 +1,16 @@
+package maisPop;
+
+import easyaccept.EasyAccept;
+
+public class Facade {
+	
+	public static void main(String[] args) {
+	    args = new String[] {
+	    		"maisPop.Facade",
+	    		"resources/Scripts de Teste/usecase_1.txt",
+	    		"resources/Scripts de Teste/usecase_2.txt",
+	    		"resources/Scripts de Teste/usecase_3.txt",
+	    		"resources/Scripts de Teste/usecase_4.txt"};
+	    EasyAccept.main(args);
+	}
+}
